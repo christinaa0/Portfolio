84b09569ec724b1d7d30c73cc2582d846429c7ba
Christina Gapy <christina.gapy@dmf.fr>
2023-11-29 17:55:07 +0100

    Nouvelle page projet avec caroussel et page veille technologique

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const experiences = document.querySelectorAll('.experience');
-    const parcoursSection = document.getElementById('parcours');
-  
-    function isElementInViewport(element) {
-      const rect = element.getBoundingClientRect();
-      return rect.top <= window.innerHeight;
-    }
-  
-    function revealExperiences() {
-      experiences.forEach((experience, index) => {
-        if (isElementInViewport(parcoursSection) && experience.classList.contains('hidden')) {
-          setTimeout(() => {
-            experience.classList.remove('hidden');
-          }, index * 500); // Décalage entre chaque expérience (0.5s par expérience)
-        }
-      });
-    }
-  
-    // Vérifier lors du défilement
-    window.addEventListener('scroll', revealExperiences);
-    revealExperiences(); // Vérifier au chargement initial
+// carousel.js
+
+function createImageCarousel(images) {
+  let currentIndex = 0;
+
+  const prevButton = document.getElementById("prev-btn");
+  const nextButton = document.getElementById("next-btn");
+  const carouselImage = document.getElementById("carousel-image");
+
+  function showImage(index) {
+      carouselImage.src = images[index];
+  }
+
+  prevButton.addEventListener("click", function() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showImage(currentIndex);
   });
-  
+
+  nextButton.addEventListener("click", function() {
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+  });
+
+  // Afficher la première image au chargement de la page
+  showImage(currentIndex);
+}
+
+
+ 
